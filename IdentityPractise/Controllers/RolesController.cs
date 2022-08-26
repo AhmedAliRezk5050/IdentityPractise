da11a469a09ab9b30e78d64e23a9cb4e73327666
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-27 00:55:35 +0200

    manage roles

---
+++ IdentityPractise/Controllers/RolesController.cs
@@ -0,0 +1,49 @@
+﻿using IdentityPractise.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityPractise.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class RolesController : Controller
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public RolesController(RoleManager<IdentityRole> roleManager)
+    {
+        _roleManager = roleManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var roles = await _roleManager.Roles.ToListAsync();
+        
+        return View(roles);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Add(RoleFormViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(nameof(Index), await FetchRoles());
+        }
+
+        if (await _roleManager.RoleExistsAsync(model.Name))
+        {
+            ModelState.AddModelError("Name", "Role exists");
+            return View(nameof(Index), await FetchRoles());
+        }
+
+        await _roleManager.CreateAsync(new IdentityRole(model.Name));
+        return RedirectToAction(nameof(Index));
+    }
+
+    private async Task<IEnumerable<IdentityRole>> FetchRoles()
+    {
+        return await _roleManager.Roles.ToListAsync();
+    }
+}
---
+++ IdentityPractise/ViewModels/RoleFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityPractise.ViewModels;
+
+public class RoleFormViewModel
+{
+    [Required, MaxLength(250)]
+    public string Name { get; set; } = null!;
+}
