2fbae92ba8060109786c60d075dcf4aa47e0e4cd
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-28 21:06:56 +0200

    admin deletes user(another approach & corrections)

---
+++ IdentityPractise/Controllers/Api/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using IdentityPractise.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityPractise.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Add(string id)
+        {
+            return Ok(new { id });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded) return Ok(new { result = "success", userName = user.UserName });
+
+            var errors = result.Errors.Select(error => error.Description).ToList();
+
+            return StatusCode(500, new { result = "failure", errors });
+        }
+    }
+}
